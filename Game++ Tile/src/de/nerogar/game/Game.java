e79c8afe8ea96485b0bc21792caef8263393e06c
Felk <studio.felk@gmail.com>
2014-05-08 23:47:55 +0200

    added test background music

--- Game++ Tile/src/de/nerogar/game/Game.java
+++ Game++ Tile/src/de/nerogar/game/Game.java
@@ -4,6 +4,9 @@ import static org.lwjgl.opengl.GL11.*;
 
 import org.lwjgl.opengl.Display;
 
+import de.nerogar.game.sound.Sound;
+import de.nerogar.game.sound.SoundManager;
+
 public class Game {
 	private RenderEngine renderEngine;
 	private Map map;
@@ -11,11 +14,16 @@ public class Game {
 	final int WIDTH = 1280;
 	final int HEIGHT = 720;
 	final int FRAMERATE = 60;
+	
+	private Sound bgMusic = SoundManager.create("licrea.wav", new Vector(0, 0));
 
 	public Game() {
 		renderEngine = new RenderEngine();
 		renderEngine.init(WIDTH, HEIGHT);
 		map = MapLoader.loadMap("map.png");
+		bgMusic.setGain(0.1f);
+		bgMusic.setLooping(true);
+		bgMusic.play();
 	}
 
 	public void run() {
@@ -26,6 +34,7 @@ public class Game {
 			Display.update();
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		}
+		SoundManager.shutdown();
 	}
 
 	private void update() {
